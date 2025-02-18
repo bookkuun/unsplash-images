b984da0256a15361b969fe52e2ebe805577e167a
bookkuun <bookkuun@gmail.com>
2025-02-18 23:28:05 +0900

    ダークモード対応

--- src/ThemeToggle.tsx
+++ src/ThemeToggle.tsx
@@ -1,5 +1,25 @@
+import { useGlobalContext } from "./context";
+import { BsFillSunFill, BsFillMoonFill } from "react-icons/bs";
+
+interface GlobalContextType {
+  isDarkTheme: boolean;
+  toggleDarkTheme: () => void;
+}
+
 const ThemeToggle = () => {
-  return <div>ThemeToggle</div>;
+  const { isDarkTheme, toggleDarkTheme } =
+    useGlobalContext() as GlobalContextType;
+  return (
+    <section className="toggle-container">
+      <button className="dark-toggle" onClick={toggleDarkTheme}>
+        {isDarkTheme ? (
+          <BsFillMoonFill className="toggle-icon" style={{ color: "#fff" }} />
+        ) : (
+          <BsFillSunFill className="toggle-icon" />
+        )}
+      </button>
+    </section>
+  );
 };
 
 export default ThemeToggle;
--- src/context.tsx
+++ src/context.tsx
@@ -1,9 +1,28 @@
-import { createContext, useContext, ReactNode } from "react";
+import { createContext, useContext, ReactNode, useState } from "react";
 
-const AppContext = createContext(null);
+interface AppContextType {
+  isDarkTheme: boolean;
+  toggleDarkTheme: () => void;
+}
+
+const AppContext = createContext<AppContextType | null>(null);
 
 export const AppProvider = ({ children }: { children: ReactNode }) => {
-  return <AppContext.Provider value={{}}>{children}</AppContext.Provider>;
+  const [isDarkTheme, setIsDarkTheme] = useState(false);
+
+  const toggleDarkTheme = () => {
+    const newDarkTheme = !isDarkTheme;
+    setIsDarkTheme(newDarkTheme);
+
+    const body = document.querySelector("body");
+    body?.classList.toggle("dark-theme", newDarkTheme);
+  };
+
+  return (
+    <AppContext.Provider value={{ isDarkTheme, toggleDarkTheme }}>
+      {children}
+    </AppContext.Provider>
+  );
 };
 
 export const useGlobalContext = () => useContext(AppContext);
