81bc5622249da2a9c1832afbb95cb77a6b871b03
bookkuun <bookkuun@gmail.com>
2025-02-19 22:59:32 +0900

    react-queryのセットアップ

--- src/Gallery.tsx
+++ src/Gallery.tsx
@@ -1,4 +1,20 @@
+import { useQuery } from "@tanstack/react-query";
+import axios from "axios";
+
+const clientId = import.meta.env.VITE_ACCESS_KEY;
+
+const url = `https://api.unsplash.com/search/photos?client_id=${clientId}&query=office`;
+
 const Gallery = () => {
+  const response = useQuery({
+    queryKey: ["images"],
+    queryFn: async () => {
+      const result = await axios.get(url);
+      return result.data;
+    },
+  });
+  console.log(response);
+
   return <div>Gallery</div>;
 };
 
--- src/SearchForm.tsx
+++ src/SearchForm.tsx
@@ -1,5 +1,30 @@
 const SearchForm = () => {
-  return <div>SearchForm</div>;
+  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    const searchValue = e.currentTarget.elements.namedItem(
+      "search"
+    ) as HTMLInputElement;
+
+    if (!searchValue || !searchValue.value) return;
+    console.log(searchValue.value);
+  };
+
+  return (
+    <section>
+      <h1 className="title">unsplash images</h1>
+      <form className="search-form" onSubmit={handleSubmit}>
+        <input
+          type="text"
+          className="form-input search-input"
+          name="search"
+          placeholder="cat"
+        />
+        <button type="submit" className="btn">
+          search
+        </button>
+      </form>
+    </section>
+  );
 };
 
 export default SearchForm;
